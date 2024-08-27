55367c3e82106ee77f2e26166c37f870cc216a2d
fxmorin <28154542+fxmorin@users.noreply.github.com>
2024-08-27 10:46:58 -0400

    Make terminal face the user when placed
    (cherry picked from commit 6743c926710b4d2c7d7f7e9306c73cacbe7a76d5)

--- src/main/java/dev/imb11/blocks/TerminalBlock.java
+++ src/main/java/dev/imb11/blocks/TerminalBlock.java
@@ -81,6 +81,6 @@ public class TerminalBlock extends BlockWithEntity {
     @Nullable
     @Override
     public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection());
     }
 }
